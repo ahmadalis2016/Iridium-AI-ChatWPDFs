46e151bddb495fbaebada69868afc6ae26811132
Ahmad Shaik <ahmadalis@gmail.com>
2024-03-01 20:51:16 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from PIL import Image
+import os
+
+# Load environment variables
+load_dotenv()
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    return text_splitter.split_text(text)
+
+def get_vector_store(text_chunks):
+    api_key = os.getenv("GOOGLE_API_KEY")
+    embeddings = GoogleGenerativeAIEmbeddings(api_key=api_key, model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context. If the answer is not available in the provided context, just say, "answer is not available in the context".\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    return load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+def process_pdf_files(pdf_docs):
+    raw_text = get_pdf_text(pdf_docs)
+    text_chunks = get_text_chunks(raw_text)
+    get_vector_store(text_chunks)
+
+def get_user_input(user_question):
+    faiss_index_path = "faiss_index/index.faiss"
+    if not os.path.exists(faiss_index_path):
+        st.error("FAISS index file not found. Make sure to upload PDF files and process them first.")
+        return ""
+
+    new_db = FAISS.load_local("faiss_index", embeddings=GoogleGenerativeAIEmbeddings(api_key=os.getenv("GOOGLE_API_KEY"), model="models/embedding-001"))
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    return response["output_text"]
+
+def main():
+    st.set_page_config("ChatPDF")
+    st.header("ChatPDF : AI-Driven Conversation")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        st.write("Reply:", get_user_input(user_question))
+
+
+
+    with st.sidebar:
+        # Load and display Iridium logo.
+        logo_path = "Images/IridiumAILogo.png" 
+        iridium_logo = Image.open(logo_path)
+        st.image(iridium_logo, use_column_width=False)
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                process_pdf_files(pdf_docs)
+                st.success("Done")
+
+if __name__ == "__main__":
+    main()
